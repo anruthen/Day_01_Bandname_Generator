65d150ea34fa20a530d7af43298c73edf2b22fe3
anruthen <61011294+anruthen@users.noreply.github.com>
2025-06-27 14:06:46 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Willkommen beim Band Namen Generator")
+stadt = input("In welcher Stadt bist du aufgewachsen?\n")
+pet = input("Wie ist der Name deines Haustiers?\n")
+print("Ein guter Name für deine Band wäre: " + stadt + " " + pet)
